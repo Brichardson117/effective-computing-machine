65b20e1d3dfa6458b577635559afdf4b76f140f6
Brittney <brittneyrichardson8@gmail.com>
2022-08-10 11:07:46 -0500

    added icons to header

--- client/src/componets/Header/index.js
+++ client/src/componets/Header/index.js
@@ -1,59 +1,74 @@
 import React from "react";
-import Typography from '@mui/material/Typography';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Link from '@mui/material/Link';
-import GlobalStyles from '@mui/material/GlobalStyles';
-import CssBaseline from '@mui/material/CssBaseline';
+import Typography from "@mui/material/Typography";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Link from "@mui/material/Link";
+import GlobalStyles from "@mui/material/GlobalStyles";
+import CssBaseline from "@mui/material/CssBaseline";
+import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 import { Button } from "@mui/material";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import "../../style.css";
 
 const Header = () => {
   return (
-<>
-<GlobalStyles styles={{ ul: { margin: 0, padding: 0, listStyle: 'none' } }} />
-<CssBaseline />
-<AppBar
-position="static"
-color="default"
-elevation={0}
-sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}`}}>
-    <Toolbar sx={{ flexWrap: 'wrap'}}>
-        <Typography variant="h6" color='inherit' noWrap sx={{ flexGrow: 1}}>
-            Company Name
-        </Typography>
-        <nav>
-        <Link
-              variant="button"
-              color="text.primary"
-              href="#"
-              sx={{ my: 1, mx: 1.5 }}
-            >
-              Features
-            </Link>
-            <Link
-              variant="button"
-              color="text.primary"
-              href="#"
-              sx={{ my: 1, mx: 1.5 }}
-            >
-              Enterprise
-            </Link>
-            <Link
-              variant="button"
-              color="text.primary"
-              href="#"
-              sx={{ my: 1, mx: 1.5 }}
-            >
-              Support
-            </Link>
+    <>
+      <GlobalStyles
+        styles={{ ul: { margin: 0, padding: 0, listStyle: "none" } }}
+      />
+      <CssBaseline />
+      <AppBar
+        position="static"
+        color="default"
+        elevation={0}
+        sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}
+      >
+        <Toolbar sx={{ flexWrap: "wrap" }}>
+          <Typography variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>
+          Material Brittney
+          </Typography>
+          <nav>
+            <ShoppingCartIcon>
+              <Link
+                variant="button"
+                color="text.primary"
+                href="#"
+                sx={{ my: 1, mx: 1.5 }}
+              >
+                Cart
+              </Link>
+            </ShoppingCartIcon>
+
+            <FavoriteIcon>
+              <Link
+                variant="button"
+                color="text.primary"
+                href="#"
+                sx={{ my: 1, mx: 1.5 }}
+              >
+                Favorites
+              </Link>
+            </FavoriteIcon>
+
+            <ChatBubbleIcon>
+              <Link
+                variant="button"
+                color="text.primary"
+                href="#"
+                sx={{ my: 1, mx: 1.5 }}
+              >
+                Support
+              </Link>
+            </ChatBubbleIcon>
+
             <Button href="#" variant="outlined" sx={{ my: 1, mx: 1.5 }}>
-            Login
-          </Button>
-        </nav>
-    </Toolbar>
-</AppBar>
-</>
+              Login
+            </Button>
+          </nav>
+        </Toolbar>
+      </AppBar>
+    </>
   );
 };
 
